cfded0394dadd84605bde5daaa42c280827ea51b
David Ostrovsky <david@ostrovsky.org>
2021-02-22 23:16:40 +0100

    Expose project config entry on project view
    Change-Id: I8c2cd96aaf69d912c791d8003979656f9c7b0e23

--- src/main/java/com/googlesource/chromium/plugins/coverage/CoverageModule.java
+++ src/main/java/com/googlesource/chromium/plugins/coverage/CoverageModule.java
@@ -6,12 +6,17 @@ package com.googlesource.chromium.plugins.coverage;
 
 import static com.google.gerrit.server.project.ProjectResource.PROJECT_KIND;
 
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.extensions.api.projects.ProjectConfigEntryType;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.restapi.RestApiModule;
 import com.google.gerrit.extensions.webui.JavaScriptPlugin;
 import com.google.gerrit.extensions.webui.WebUiPlugin;
+import com.google.gerrit.server.config.ProjectConfigEntry;
 
 public class CoverageModule extends RestApiModule {
+
+  static final String KEY_CODE_COVERAGE = "code-coverage";
 
   @Override
   protected void configure() {
@@ -21,5 +26,17 @@ public class CoverageModule extends RestApiModule {
 
     // Register the config endpoint used by the JavaScript client code.
     get(PROJECT_KIND, "config").to(GetConfig.class);
+
+    // Configure UI element to be exposed on the project view
+    bind(ProjectConfigEntry.class)
+        .annotatedWith(Exports.named(KEY_CODE_COVERAGE))
+        .toInstance(
+            new ProjectConfigEntry(
+                "Show Code Coverage",
+                "false",
+                ProjectConfigEntryType.BOOLEAN,
+                null,
+                false,
+                "Show code coverage in diff view."));
   }
 }
